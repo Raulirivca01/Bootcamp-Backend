24842696a876a24ff792ee10de64e9faf3d337b0
Raul Carlos Abelardo Rivera Castañeda <raulriverac25@outlook.com>
2022-09-25 21:49:05 -0500

    Creacion del app y 1era parte del CRUD"

---
+++ Bootcamp.ViewModel/PersonViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bootcamp.ViewModel
+{
+    public class PersonViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string DocumentTypeShortName { get; set; }
+        public string DocumentNumber { get; set; }
+    }
+}
